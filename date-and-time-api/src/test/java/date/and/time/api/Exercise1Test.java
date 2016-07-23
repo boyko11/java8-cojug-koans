8ab20ccbe3893c1b5fcb26274327c46f450aa6c0
Boyko Todorov <boyko11@gmail.com>
2016-07-22 21:21:02 -0400

    Excercise1 Dates

--- date-and-time-api/src/test/java/date/and/time/api/Exercise1Test.java
+++ date-and-time-api/src/test/java/date/and/time/api/Exercise1Test.java
@@ -1,17 +1,17 @@
 package date.and.time.api;
 
-import common.test.tool.annotation.Necessity;
-import common.test.tool.dataset.DateAndTimes;
-
-import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
 import java.time.temporal.TemporalAdjusters;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import common.test.tool.annotation.Necessity;
+import common.test.tool.dataset.DateAndTimes;
 
 public class Exercise1Test {
 
@@ -21,7 +21,7 @@ public class Exercise1Test {
         /**
          * Create a {@link LocalDate} of 2015-06-18 by using {@link LocalDate#of}
          */
-        LocalDate localDate = null;
+        LocalDate localDate = LocalDate.of(2015, 6, 18);
 
         assertThat(localDate.toString(), is("2015-06-18"));
     }
@@ -32,7 +32,8 @@ public class Exercise1Test {
         /**
          * Create a {@link LocalDate} of 2015-06-18 from String by using {@link LocalDate#parse}
          */
-        LocalDate localDate = null;
+    	LocalDate localDate = LocalDate.parse("2015-06-18");
+    	//LocalDate localDate = LocalDate.parse("2015-06-18", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
         assertThat(localDate.toString(), is("2015-06-18"));
     }
@@ -46,7 +47,7 @@ public class Exercise1Test {
          * Create a {@link LocalDate} from {@link ld} with year 2015
          * by using {@link LocalDate#withYear} or {@link LocalDate#with}
          */
-        LocalDate localDate = null;
+        LocalDate localDate = ld.with(ld);
 
         assertThat(localDate.getYear(), is(2015));
         assertThat(localDate.getMonth(), is(ld.getMonth()));
@@ -62,7 +63,7 @@ public class Exercise1Test {
          * Create a {@link LocalDate} from {@link ld} adjusted into first day of next year
          * by using {@link LocalDate#with} and {@link TemporalAdjusters#firstDayOfNextYear}
          */
-        LocalDate localDate = null;
+        LocalDate localDate = ld.with(TemporalAdjusters.firstDayOfNextYear());
 
         assertThat(localDate.getYear(), is(ld.getYear() + 1));
         assertThat(localDate.getMonth(), is(Month.JANUARY));
@@ -78,7 +79,7 @@ public class Exercise1Test {
          * Create a {@link LocalDate} from {@link ld} with 10 month later
          * by using {@link LocalDate#plusDays} or {@link LocalDate#plus}
          */
-        LocalDate localDate = null;
+        LocalDate localDate = ld.plusMonths(10);
 
         assertThat(localDate.getYear(), is(ld.getYear() + 1));
         assertThat(localDate.getMonth(), is(Month.APRIL));
@@ -94,7 +95,7 @@ public class Exercise1Test {
          * Create a {@link LocalDate} from {@link ld} with 10 days before
          * by using {@link LocalDate#minusDays} or {@link LocalDate#minus}
          */
-        LocalDate localDate = null;
+        LocalDate localDate = ld.minusDays(10);
 
         assertThat(localDate.getYear(), is(ld.getYear()));
         assertThat(localDate.getMonth(), is(ld.getMonth()));
@@ -111,8 +112,8 @@ public class Exercise1Test {
          * Define a {@link Period} of 1 year 2 month 3 days
          * Create a {@link LocalDate} adding the period to {@link ld} by using {@link LocalDate#plus}
          */
-        Period period = null;
-        LocalDate localDate = null;
+        Period period = Period.of(1, 2, 3);
+        LocalDate localDate = ld.plus(period);
 
         assertThat(localDate.getYear(), is(ld.getYear() + 1));
         assertThat(period.getMonths(), is(2));
@@ -122,6 +123,7 @@ public class Exercise1Test {
     @Test
     @Necessity(true)
     public void localDateIsAfter() {
+    	
         LocalDate ld = DateAndTimes.LD_20150618;
         LocalDate ld2 = DateAndTimes.LD_20150807;
 
@@ -129,7 +131,7 @@ public class Exercise1Test {
          * Check whether {@link ld2} is after {@link ld} or not
          * by using {@link LocalDate#isAfter} or {@link LocalDate#isBefore}
          */
-        boolean isAfter0618 = false;
+        boolean isAfter0618 = ld2.isAfter(ld);
 
         assertThat(isAfter0618, is(true));
     }
@@ -137,6 +139,7 @@ public class Exercise1Test {
     @Test
     @Necessity(true)
     public void localDateUntil() {
+    	
         LocalDate ld = DateAndTimes.LD_20150618;
         LocalDate ld2 = DateAndTimes.LD_20150807;
 
@@ -144,7 +147,7 @@ public class Exercise1Test {
          * Create a period from {@link ld} till {@link ld2}
          * by using {@link LocalDate#until}
          */
-        Period period = null;
+        Period period = Period.between(ld, ld2);
 
         assertThat(period.getYears(), is(0));
         assertThat(period.getMonths(), is(1));
