b81599bd16f6b5d4a452d99bd5675ec915829191
Boyko Todorov <boyko11@gmail.com>
2016-07-23 17:45:02 -0400

    Exercise 6 - Stream API

--- stream-api/src/test/java/stream/api/Exercise6Test.java
+++ stream-api/src/test/java/stream/api/Exercise6Test.java
@@ -1,25 +1,26 @@
 package stream.api;
 
-import common.test.tool.annotation.Necessity;
-
-import org.junit.Test;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import common.test.tool.annotation.Necessity;
 
 public class Exercise6Test {
 
     @Test
     @Necessity(true)
     public void streamFromValues() {
+    	
         /**
          * Create a stream with string values "a" "b" "c" by using {@link Stream#of}
          */
-        Stream<String> abcStream = null;
+        Stream<String> abcStream = Stream.of("a", "b", "c");
 
         List<String> abcList = abcStream.collect(Collectors.toList());
         assertThat(abcList, contains("a", "b", "c"));
@@ -28,10 +29,11 @@ public class Exercise6Test {
     @Test
     @Necessity(true)
     public void numberStream() {
+    	
         /**
          * Create a stream only with multiples of 3, starting from 0, size of 10, by using {@link Stream#iterate}
          */
-        Stream<Integer> numbers = null;
+        Stream<Integer> numbers = Stream.iterate(0, (int1) -> int1 + 3).limit(10);
 
         List<Integer> numbersList = numbers.collect(Collectors.toList());
         assertThat(numbersList, contains(0, 3, 6, 9, 12, 15, 18, 21, 24, 27));
