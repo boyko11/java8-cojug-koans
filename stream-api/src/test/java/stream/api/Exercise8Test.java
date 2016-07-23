e2371f63057f8f2a634d5d192010d8c4b5112cc9
Boyko Todorov <boyko11@gmail.com>
2016-07-23 19:17:46 -0400

    Exercise 8 - stream api

--- stream-api/src/test/java/stream/api/Exercise8Test.java
+++ stream-api/src/test/java/stream/api/Exercise8Test.java
@@ -8,9 +8,12 @@ import common.test.tool.entity.Shop;
 
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.hasItems;
@@ -28,8 +31,9 @@ public class Exercise8Test extends ClassicOnlineStore {
         /**
          * Create a set of item names that are in {@link Customer.wantToBuy} but not on sale in any shop.
          */
-        List<String> itemListOnSale = null;
-        Set<String> itemSetNotOnSale = null;
+        Set<String> itemSetOnSale = shopStream.flatMap((shop) -> shop.getItemList().stream()).map(Item::getName).collect(Collectors.toSet());
+        Set<String> itemSetNotOnSale = customerStream.flatMap((customer) -> customer.getWantToBuy().stream()).map(Item::getName).collect(Collectors.toSet());
+        itemSetNotOnSale.removeAll(itemSetOnSale);
 
         assertThat(itemSetNotOnSale, hasSize(3));
         assertThat(itemSetNotOnSale, hasItems("bag", "pants", "coat"));
@@ -38,6 +42,7 @@ public class Exercise8Test extends ClassicOnlineStore {
     @Test
     @Necessity(false)
     public void havingEnoughMoney() {
+    	
         Stream<Customer> customerStream = this.mall.getCustomerList().stream();
         Stream<Shop> shopStream = this.mall.getShopList().stream();
 
@@ -46,9 +51,19 @@ public class Exercise8Test extends ClassicOnlineStore {
          * Items that are not on sale can be counted as 0 money cost.
          * If there is several same items with different prices, customer can choose the cheapest one.
          */
-        List<Item> onSale = null;
-        Predicate<Customer> havingEnoughMoney = null;
-        List<String> customerNameList = null;
+        List<Item> onSale = shopStream.flatMap((shop) -> shop.getItemList().stream()).collect(Collectors.toList());
+        Predicate<Customer> havingEnoughMoney = (customer) -> {
+        	
+        	int totalWantedCost = 0;
+        	for(Item itemWantToBuy : customer.getWantToBuy()) {
+        		List<Item> itemsOnSale = onSale.stream().filter((item) -> item.getName().equalsIgnoreCase(itemWantToBuy.getName())).collect(Collectors.toList());
+        		if(itemsOnSale != null && itemsOnSale.isEmpty() == false) {
+        			totalWantedCost += itemsOnSale.stream().min(Comparator.comparing(Item::getPrice)).get().getPrice();
+        		}
+        	}
+        	return totalWantedCost <= customer.getBudget();
+        };
+        List<String> customerNameList = customerStream.filter(havingEnoughMoney).map(Customer::getName).collect(Collectors.toList());
 
         assertThat(customerNameList, hasSize(7));
         assertThat(customerNameList, hasItems("Joe", "Patrick", "Chris", "Kathy", "Alice", "Andrew", "Amy"));
